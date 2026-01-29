b02885a3ed26940b486f4a9676d9e75671363c85
LoubnaB <boukateloubna@gmail.com>
2026-01-29 18:17:20 +0100

    Configuration SERVICE REGISTRY (EUREKA SERVER)

---
+++ eureka-server/src/main/java/com/academique/eureka_server/EurekaServerApplication.java
@@ -0,0 +1,17 @@
+package com.academique.eureka_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+
+@SpringBootApplication
+//ACTIVER EUREKA SERVER  -  Sans @EnableEurekaServer → Eureka NE MARCHE PAS
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+
+}
